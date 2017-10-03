508820c83297343d7c6d408eb950f6cc7e1cd966
Yanik-Dev <yanikblake@live.com>
2017-10-03 14:25:29 -0500

    first commit

---
+++ views/login.php
@@ -0,0 +1,9 @@
+
+<form action="" method="get">
+    <label for="email-input"></label>
+    <input type="text" name="email" id="email-input">
+    <label for="password-input"></label>
+    <input type="password" name="password" id="password-input">
+    <button type="submit">login</button>
+    <button type="submit">Sign up</button>
+</form>
