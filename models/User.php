6ce59f6ea0d46eed10132b01468b27ec73805b67
Yanik-Dev <yanikblake@live.com>
2017-10-08 22:32:45 -0500

    added user domain class

--- models/User.php
+++ models/User.php
@@ -1,4 +1,43 @@
 <?php
+/**
+ * domain class for user
+ */
 class User{
-    
+    private $userId;
+    private $username;
+    private $password;
+    private $firstname;
+    private $lastname;
+    private $userType;
+    private $salt;
+    private $dateCreated;
+    private $dateUpdated;
+    private $isAccountVerified;
+    private $resetToken;
+
+    //accessors
+    public function getUserId(){ return $this->userId; }
+    public function getUsername(){ return $this->username; }
+    public function getPassword(){ return $this->password; }
+    public function getFirstname(){ return $this->firstname; }
+    public function getLastname(){ return $this->lastname; }
+    public function getUserType(){ return $this->userType; }
+    public function getSalt(){ return $this->salt; }
+    public function getDateCreated() { return $this->dateCreated; }
+    public function getDateUpdated(){ return $this->dateUpdated; }
+    public function getResetToken(){ return $this->resetToken; }
+    public function getIsAccountVerified(){ return $this->isAccountVerified; }
+
+    //mutators
+    public function setUserId($userId){ $this->userId = $userId; }
+    public function setUsername($username){ $this->username = $username; }
+    public function setPassword($password){  $this->password = $password; }
+    public function setFirstname($firstname){  $this->firstname = $firstname; }
+    public function setLastname($lastname){  $this->lastname = $lastname; }
+    public function setUserType($userType){  $this->userType = $userType; }
+    public function setSalt($salt){ $this->salt = $salt; }
+    public function setDateCreated($dateCreated) { $this->dateCreated = $dateCreated; }
+    public function setDateUpdated($dateUpdated){  $this->dateUpdated= $dateUpdated; }
+    public function setIsAccountVerified($isAccountVerified){  $this->isAccountVerified = $isAccountVerified; }
+    public function setResetToken($token){ return $this->resetToken = $token; }
 }
