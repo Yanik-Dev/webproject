a3425ee6c21a241ea6eafcc596133c4524ef5041
unknown <rhaynharley@yahoo.com>
2017-10-09 19:32:26 -0500

    getters and setters

---
+++ models/Addresses.php
@@ -0,0 +1,68 @@
+<?php
+
+class
+
+{
+
+private $fk_businessID;
+private $street;
+private $city;
+private $province;
+
+
+public Function getFk_businessID()
+{
+Return this->fk_businessID;
+}
+
+
+public Function setfk_businessID(fk_businessID)
+{
+this->fk_businessID=fk_businessID;
+}
+
+
+
+public Function getStreet()
+{
+Return this->street;
+}
+
+
+public Function setStreet(street)
+{
+this->street=street;
+}
+
+
+
+public Function get()
+{
+Return this->city;
+}
+
+public Function set(city)
+{
+this->city=city;
+}
+
+
+
+public Function get()
+{
+Return this->province;
+}
+
+
+public Function set(province)
+{
+this->province=province;
+}
+
+}
+
+
+
+
+
+?>
