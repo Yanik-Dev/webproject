f2bc43299ffe4da96f802e2c84e8859d6858a14e
Yanik-Dev <yanikblake@live.com>
2017-10-19 12:07:19 -0500

    added success page

---
+++ public/success.php
@@ -0,0 +1,19 @@
+<?php 
+ $title = "Home";
+ include '../includes/header.php';
+ include '../includes/public-nav.php';
+?>
+<div class="ui middle aligned centered aligned grid" id="registrationModule">
+  <div class="column">
+  <div class="ui segment">
+    <h2 class="ui icon header">
+    <i class="user circle icon"></i>
+    <div class="content">
+        You have been successfully registered...
+        <p class="sub header">Please proceed to verify your email address.</p>
+        <br />
+        <button class="ui button">Proceed to Login</button>
+    </div>
+    </h2>
+  </div>
+</div>
