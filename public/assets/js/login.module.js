d43f57c73032c62d5eba585be50b5501eef0b81c
Yanik-Dev <yanikblake@live.com>
2017-10-18 23:43:14 -0500

    implemented login module

--- public/assets/js/login.module.js
+++ public/assets/js/login.module.js
@@ -1,13 +1,42 @@
 let LoginModule = (function(){
-    console.log("init")
     //cache DOM
     let $el = $('#loginModule');
-    let $loginForm = $('#login-form');
-
+    let $loginForm = $('.ui.form');
+    let $errorMsg = $el.find('#error-msg');
     //bind events
     $loginForm.on('submit', _authenticate);
+    
+    //init
+    $loginForm.form({
+        on: 'blur',
+        fields: {
+          email: {
+            identifier: 'email',
+            rules: [
+              {
+                type   : 'email',
+                prompt : 'Please enter a valid email'
+              }
+            ]
+          },
+          password: {
+            identifier: 'password',
+            rules: [
+              {
+                type   : 'minLength[8]',
+                prompt : 'Your password must be at least {ruleValue} characters'
+              }
+            ]
+          },
+        }
+    });
 
+    //event handlers
     function _authenticate(event){
+        if( !$loginForm.form('is valid') ){
+            $errorMsg.hide();
+            return;
+        }
         $.ajax({
             url: './actions/login.php',
             dataType: 'text',
@@ -21,11 +50,21 @@ let LoginModule = (function(){
     }
 
     function _onLogin(data){
-        console.log(data)
+        let result = JSON.parse(data);
+        if(result.status == 200){
+            $errorMsg.hide();
+            location.href=".././index.php";
+        }else{
+            $errorMsg.text("Incorrect email or password");
+            $errorMsg.show();  
+        }
     }
 
     function _onError( jqXhr, textStatus, errorThrown ){
         console.log( errorThrown );
+        $errorMsg.text("An unexpected error as occured.");
+        $errorMsg.show();
     }
 
 })();
+
