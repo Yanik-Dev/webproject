2ebcd6d830bdc709403470019ad4e96c424534e7
yanik-dev <yanikblake@live.com>
2017-12-08 11:26:08 -0500

    implement base validations

--- public/import.php
+++ public/import.php
@@ -14,13 +14,13 @@ if($path=='' || !is_numeric($businessId)){
     header('Location: ./offerings.php');
 }
 
-$table = [];
-$rowCount = 0;
-
+//declarations
+$table = $categories = $errors = $offerings = [];
+$rowCount = $failedCount = $successCount  = 0;
+$isSafe = true;
 $business =new Business();
 $category=new OfferingCategory();
 $offering=new Offering();
-
 
 //set Category Object
 $category->setId('');
@@ -39,16 +39,15 @@ $offering->setCategory($category);
 
 $offeringsList = OfferingService::findAll($offering, ['start'=>0,'limit'=>0]);
 $categoriesList = OfferingCategoryService::findAll();
-$categories = $offerings = [];
+
 
 foreach($offeringsList as $offering){
-    $offerings[] = $offering["name"];
+    $offerings[] = strtolower($offering["name"]);
 }
 foreach($categoriesList as $category){
     $categories[] = $category["category"];
 }
 
-$errors = [];
 try{
     $objPHPExcel = PHPExcel_IOFactory::load($path);
 }catch(Exception $e){
@@ -62,7 +61,7 @@ if(isset($objPHPExcel)){
         $rowCount           = $highestRow;
         $highestColumn      = $worksheet->getHighestColumn(); 
         $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
-        $nrColumns = ord($highestColumn) - 64;
+        $nrColumns          = ord($highestColumn) - 64;
         if($worksheetTitle != "Items"){
             $errors[] = "Items worksheet not found";
             break;
@@ -82,7 +81,7 @@ if(isset($objPHPExcel)){
 
 <div class="ui centered padded grid">
     <div class="sixteen column" >
-        <a href="./offerings.php" class="ui labeled icon button">
+        <a href="./offerings.php"  id="top" class="ui labeled icon button">
             <i class="left chevron icon"></i>
             Back
         </a>
@@ -99,7 +98,15 @@ if(isset($objPHPExcel)){
                     Download Template
                 </button>
             </div>
-        <?php  exit; endif; ?>
+        
+       <?php  exit; else: ?>
+            <?php if(count($table) > 12):?>
+                <a href="#stats" class="ui primary labeled icon button">
+                    <i class="bar chart icon"></i>
+                    Results
+                </a>
+            <?php endif; ?>
+        <?php endif; ?>
         
         <table class="ui celled table">
             <thead>
@@ -112,38 +119,89 @@ if(isset($objPHPExcel)){
             </thead>
 
             <tbody>
-                <?php for ($row = 2; $row <= $rowCount; ++ $row):?>
+                <?php 
+                   for ($row = 2; $row <= $rowCount; ++ $row):
+                    $isSafe = true;
+                    $isEmpty = false;
+                    ?>
                     <tr>
                     <?php for ($col = 0; $col < 5; ++ $col):?>
                         <?php 
-                        $exist = 'positive';
-                        $invalidService = false;
+                        $exist=false;
                         if($col != 3):
                             if($col==0):
-                                if(in_array($table[$row][$col], $offerings)):
-                                    $exist="error";
-                                elseif(in_array($table[$row][$col], $categories)):
-                                    $invalidService = true;
+                                if(trim($table[$row][$col]) ==''):
+                                    $isSafe = false;
+                                    $isEmpty = true;
+                                    break;
+                                elseif(in_array(strtolower($table[$row][$col]), $offerings)):
+                                    $exist=true;
+                                    $isSafe = false;
                                 else:
-                                    //perpare for insert;
-
+                                    $offerings[] = strtolower($table[$row][$col]);
                                 endif;
                             endif;
-                            if($exist == "error")
-                                
-                                echo '<td class="'.$exist.'"> <del> '.$table[$row][$col].'</del></td>';
-                            else if($invalidService):
-                                echo '<td class="error">  <del> '.$table[$row][$col].'</del></td>';
+                            if($exist):
+                                echo '<td class="error" data-tooltip="Item \''.$table[$row][$col].'\' already exist" data-position="top left"> <del> '.$table[$row][$col].'</del></td>';
+                            elseif($col == 4 && !in_array($table[$row][$col], $categories)):
+                                    $isSafe = false;
+                                    echo '<td class="error" data-tooltip="\''.$table[$row][$col].'\' is not a valid category" data-position="top left">  <del> '.$table[$row][$col].'</del></td>';
+                            elseif($col == 2 && !is_numeric($table[$row][$col])):
+                                $isSafe = false;
+                                echo '<td class="error" data-tooltip="Expecting a numeric value" data-position="top left">  <del> '.$table[$row][$col].'</del></td>';
                             else:
-                                echo '<td class="'.$exist.'">'.(($col == 0)?'<i class="icon checkmark"></i>':'').' '.$table[$row][$col].'</td>';
+                                echo '<td class="positive">'.(($col == 0)?'<i class="icon checkmark"></i>':'').' '.$table[$row][$col].'</td>';
                             endif;
                         endif; 
                         ?>
                     <?php endfor; ?>
-                    
+
+                    <?php 
+                        if($isSafe){
+                            
+                            $newOffering = new Offering();
+                            $business =new Business();
+                            $category=new OfferingCategory();
+
+                        
+                            foreach($categoriesList as $c){
+                                if($c["category"] == $table[$row][3] ){
+                                    //set Category Object
+                                    $category->setId($categoryId);
+                                    break;
+                                }
+                            }
+                            //set Business Object
+                            $business->setId($businessId);
+
+                            //set Offering Object
+                            $newOffering->setName($table[$row][0]);
+                            $newOffering->setCost($table[$row][2]);
+                            $newOffering->setDescription($table[$row][1]);
+                            $newOffering->setBusiness($business);
+                            $newOffering->setCategory($category);
+                            $successCount++;
+                        }elseif(!$isEmpty){
+                            $failedCount++;
+                        }
+                    ?>
                     </tr>
-                <?php endfor; ?>
+            <?php endfor; ?>
             </tbody>
         </table>
+        <div class="ui padded segment" id="stats">
+          <p class="green"><i class="icon checkmark"></i> Successful Inserts: <?= $successCount ?></p>
+          <p class="red"><i class="icon remove"></i> Unsuccessful: <?= $failedCount ?></p>
+         <?php if(($failedCount + $successCount) > 12):?>
+            <a href="./offerings.php"  id="top" class="ui labeled icon button">
+                <i class="left chevron icon"></i>
+                Back
+            </a>
+            <a href="#top" class="ui labeled icon button">
+                <i class="chevron up icon"></i>
+                Back To Top
+            </a>
+         <?php endif;?>
+        </div>
    </div>
 </div>
